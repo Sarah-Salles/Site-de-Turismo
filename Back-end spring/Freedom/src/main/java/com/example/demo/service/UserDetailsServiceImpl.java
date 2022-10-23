373ac11ea157838c01b22a2a0c2341918000f683
Sarah-Salles <sarahsallesfc@gmail.com>
2022-10-23 12:00:41 -0300

    Erro corrigido

--- Back-end spring/Freedom/src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ Back-end spring/Freedom/src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -1,4 +1,4 @@
-package com.exemple.demo.service;
+package com.example.demo.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
